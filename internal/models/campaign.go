0b361e2dbb080cf235f2c6f367c4aa3a859a38f2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-04-05 22:07:13 +0530

    [refactor]: added foriegn key

--- internal/models/campaign.go
+++ internal/models/campaign.go
@@ -10,7 +10,13 @@ type Campaign struct {
 	ID        uint           `gorm:"primarykey" json:"id"`
 	Name      string         `gorm:"uniqueIndex;size:255" json:"name"`
 	Role      string         `gorm:"size:50;default:user" json:"role"`
+	Contacts  []*Contact     `gorm:"many2many:campaign_audiences;" json:"contacts"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+type CampaignAudience struct {
+	CampaignID uint `gorm:"primaryKey" json:"campaign_id"`
+	ContactID  uint `gorm:"primaryKey" json:"contact_id"`
 }
