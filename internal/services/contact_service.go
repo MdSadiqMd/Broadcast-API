4c1a7c14217d58cc218cb4dac9578ded58f84052
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-04-06 00:41:50 +0530

    [feat]: contact services added

---
+++ internal/services/contact_service.go
@@ -0,0 +1,57 @@
+package services
+
+import (
+	"github.com/MdSadiqMd/Broadcast-API/internal/models"
+	"github.com/MdSadiqMd/Broadcast-API/internal/repositories"
+	"gorm.io/gorm"
+)
+
+type ContactService struct {
+	repo *repositories.ContactRepository
+}
+
+func NewContactService(db *gorm.DB) *ContactService {
+	return &ContactService{
+		repo: repositories.NewContactRepository(db),
+	}
+}
+
+func (s *ContactService) CreateContact(contact *models.Contact) (*models.Contact, error) {
+	createdContact, err := s.repo.CreateContact(contact)
+	if err != nil {
+		return nil, err
+	}
+	return &createdContact, nil
+}
+
+func (s *ContactService) GetAllContacts(audienceId uint) ([]models.Contact, error) {
+	contacts, err := s.repo.GetAllContacts(audienceId)
+	if err != nil {
+		return nil, err
+	}
+	return contacts, nil
+}
+
+func (s *ContactService) GetContactByID(id uint) (*models.Contact, error) {
+	contact, err := s.repo.GetContactByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return contact, nil
+}
+
+func (s *ContactService) UpdateContact(contact *models.Contact) (*models.Contact, error) {
+	updatedContact, err := s.repo.UpdateContact(contact)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedContact, nil
+}
+
+func (s *ContactService) DeleteContact(id uint) error {
+	err := s.repo.DeleteContact(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
