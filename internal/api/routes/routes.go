16ee84bd27d09259c57eb820a5a468d0ddf7b23f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-04-17 16:58:52 +0530

    [feat]: mail routes configured

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -7,6 +7,8 @@ import (
 	"github.com/MdSadiqMd/Broadcast-API/internal/api/handlers"
 	appMiddleware "github.com/MdSadiqMd/Broadcast-API/internal/api/middleware"
 	"github.com/MdSadiqMd/Broadcast-API/internal/services"
+	"github.com/MdSadiqMd/Broadcast-API/pkg/config"
+	"github.com/MdSadiqMd/Broadcast-API/pkg/email"
 	"github.com/go-chi/chi/v5"
 	"gorm.io/gorm"
 )
@@ -17,6 +19,21 @@ func Setup(r *chi.Mux, db *gorm.DB, jwtSecret string) {
 	contactService := services.NewContactService(db)
 	broadcastService := services.NewBroadcastService(db)
 
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+	smtpClient := email.NewSMTPClient(email.SMTPConfig{
+		Host:     cfg.SMTP.Host,
+		Port:     cfg.SMTP.Port,
+		Username: cfg.SMTP.Username,
+		Password: cfg.SMTP.Password,
+		FromName: cfg.SMTP.FromName,
+		FromAddr: cfg.SMTP.FromAddr,
+		UseTLS:   false,
+	})
+
+	mailService := services.NewMailService(db, smtpClient)
 	auth := appMiddleware.NewAuth(appMiddleware.AuthConfig{
 		JWTSecret:     jwtSecret,
 		TokenDuration: 24 * time.Hour,
@@ -27,12 +44,14 @@ func Setup(r *chi.Mux, db *gorm.DB, jwtSecret string) {
 	compaignHandler := handlers.NewCampaignHandler(compaignService, auth)
 	contactHandler := handlers.NewContactHandler(contactService, auth)
 	broadcastHandler := handlers.NewBroadcastHandler(broadcastService, auth)
+	mailHandler := handlers.NewMailHandler(mailService, auth)
 
 	r.Use(auth.Middleware())
 
 	r.Route("/api", func(r chi.Router) {
 		r.Post("/login", authHandler.Login)
 		r.Post("/register", authHandler.Register)
+
 		// Protected Routes
 		r.Group(func(r chi.Router) {
 			r.Use(auth.Middleware())
@@ -53,6 +72,13 @@ func Setup(r *chi.Mux, db *gorm.DB, jwtSecret string) {
 			r.Get("/broadcasts", broadcastHandler.ListBroadcasts)
 			r.Post("/broadcast/{id}/send", broadcastHandler.SendBroadcast)
 			r.Delete("/broadcast/{id}", broadcastHandler.DeleteBroadcast)
+
+			r.Post("/mail/test", mailHandler.SendTestEmail)
+			r.Post("/mail/transactional", mailHandler.SendTransactionalEmail)
+			r.Post("/mail/job/{id}/process", mailHandler.ProcessEmailJob)
+			r.Post("/mail/campaign/send", mailHandler.ProcessCampaignEmail)
+			r.Post("/mail/campaign/bulk", mailHandler.BulkSendCampaign)
+
 			// Admin Routes
 			r.Group(func(r chi.Router) {
 				r.Use(appMiddleware.RequireRole("admin"))
